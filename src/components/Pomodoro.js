2149fb2dcf39f3c62e497170b89deba6139dc6d5
maknetaRo <makneta@gmail.com>
2021-01-13 23:05:38 +0100

    started working on start button

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Moment from 'react-moment';
 import moment from 'moment';
 
@@ -7,13 +7,23 @@ import moment from 'moment';
 const Pomodoro = () => {
 
     const [minutes, setMinutes] = useState(25)
+    const [seconds, setSeconds] = useState(0)
 
-    let timeDisplay = moment().minute(minutes).second(0);
+    let timeDisplay = moment().minute(minutes).second(seconds);
     console.log(timeDisplay)
 
-    const [counter, setCounter] = useState(0)
 
-       return (
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if ((minutes > 0) && (seconds > 0)) {
+                setSeconds(seconds - 1)
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+
+    }, []);
+
+    return (
         <div>
             <h1>Pomodoro clock</h1>
 
@@ -29,7 +39,7 @@ const Pomodoro = () => {
                     </div>
                 </div>
                 <div class="buttons">
-                    <button class="btn btn-start"></button>
+                    <button class="btn btn-start" onClick={() => setSeconds(seconds - 1)}></button>
                     <button class="btn btn-pause"></button>
                     <button class="btn btn-stop" onClick={() => setMinutes(25)}></button>
                 </div>
